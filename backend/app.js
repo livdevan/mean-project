625be317d1838bd9c4686dd413b66b02813f9cb5
Livan Hagi Osman <lhagiosman@amiltone.com>
2019-05-16 15:56:20 +0200

    adds mongoose, post backend model and  POST refactoring

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 
+const Post = require("./models/post");
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
@@ -19,7 +21,10 @@ app.use((req, res, next) => {
 });
 
 app.post("/posts", (req, res, next) => {
-  const post = req.body;
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content
+  });
   console.log(post);
   res.status(201).json({
     message: "Post added successfully"
